b23479c28aeb589fe50b0e35d048e83085ed51d3
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-11-08 16:15:23 +0700

    Update quick build script

--- Assets/JANOARG.Chartmaker/Scripts/Editor/QuickBuild.cs
+++ Assets/JANOARG.Chartmaker/Scripts/Editor/QuickBuild.cs
@@ -1,14 +1,24 @@
+using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 using UnityEditor;
 using UnityEngine;
 
 namespace JANOARG.Chartmaker.Editor
 {
+
     public class QuickBuild
     {
+
+#if UNITY_EDITOR_LINUX
+        [DllImport("libc", SetLastError = true)]
+        private static extern int chmod(string pathname, int mode);
+#endif
+
         [MenuItem("JANOARG/Quick Build", priority = 1000)]
-        public static void Build ()
+        public static void Build()
         {
             string path = Path.Combine(Path.GetDirectoryName(Application.dataPath)!, "Builds");
 
@@ -16,13 +26,15 @@ namespace JANOARG.Chartmaker.Editor
             Directory.CreateDirectory(path);
 
             // Build for Windows
-            BuildPipeline.BuildPlayer(new BuildPlayerOptions() {
+            BuildPipeline.BuildPlayer(new BuildPlayerOptions()
+            {
                 locationPathName = Path.Combine(path, "Chartmaker-win-x86_64/Chartmaker.exe"),
                 target = BuildTarget.StandaloneWindows64
             });
 
             // Build for Linux
-            BuildPipeline.BuildPlayer(new BuildPlayerOptions() {
+            BuildPipeline.BuildPlayer(new BuildPlayerOptions()
+            {
                 locationPathName = Path.Combine(path, "Chartmaker-linux-x86_64/Chartmaker.x86_64"),
                 target = BuildTarget.StandaloneLinux64
             });
@@ -36,12 +48,15 @@ namespace JANOARG.Chartmaker.Editor
                 "tar -czvf Chartmaker-linux-x86_64.tar.gz Chartmaker-linux-x86_64/",
                 "zip -r Chartmaker-win-x86_64.zip Chartmaker-win-x86_64/*",
             });
+
+            chmod(scriptPath, Convert.ToInt32("755", 8));
+
             var process = new Process()
             {
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = "/bin/bash",
-                    Arguments = $"-c \"{scriptPath.Replace("\"", "\\\"")}\"",
+                    Arguments = $"-c {scriptPath.Replace(" ", "\\\\\\ ")}",
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
                     UseShellExecute = false,
@@ -53,10 +68,14 @@ namespace JANOARG.Chartmaker.Editor
             UnityEngine.Debug.Log(process.StandardError.ReadToEnd());
             process.WaitForExit();
             UnityEngine.Debug.Log("zip exit code = " + process.ExitCode);
-            if (process.ExitCode == 0) 
+            if (process.ExitCode == 0)
+            {
                 File.Delete(scriptPath);
-            else 
+            }
+            else
+            {
                 UnityEngine.Debug.LogWarning("zip.sh execution failed - please run the script file manually");
+            }
 #endif
 
             Application.OpenURL("file://" + path);
